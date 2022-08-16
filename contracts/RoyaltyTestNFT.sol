82b74c81621f902d9371b7891faa77e836c0e1f8
Bulalu <elishabulalu031@gmail.com>
2022-08-16 16:44:21 +0300

    clean up

--- contracts/RoyaltyTestNFT.sol
+++ contracts/RoyaltyTestNFT.sol
@@ -121,15 +121,8 @@ contract OrbofiTestNFT is ERC721Enumerable, Ownable, ERC2981 {
 		require(sent, 'Failed to withdraw BNB');
 	}
 
-    /**
-	 * Updates the address receiving royalties
-	 * @notice only owner can withdraw
-	 */
-
-    function changeRoyaltyReceiver(address receiver) external onlyOwner {
-		_changeRoyaltyReceiver(receiver);
-	}
-
+   
+  
 
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
         require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token.");
